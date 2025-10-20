5dc2c862e5cc7e84ddd83eec111acaef36fe1783
Kwame Atuahene <atuahene.enoch777@gmail.com>
2025-10-20 02:17:03 -0500

    small fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Header from './components/Header'
-import Hero from './components/Hero'
-import About from './components/About'
-import Projects from './components/Projects'
-import Skills from './components/Skills'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
       </main>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
